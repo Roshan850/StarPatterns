1128c8f071527a10d6145e9c14501ee76d00fbeb
Roshan kumar Jha <jharoshankumar850@gmail.com>
2025-09-03 22:14:44 +0530

    First
    same

--- HollowRectangle.java
+++ HollowRectangle.java
@@ -1,3 +1,23 @@
+class Code {
+    public void hollowRectangle(int toRow, int toCol) {
+        for(int i = 1;i<=toRow;i++){
+            for (int j = 1; j <= toCol; j++) {
+                if (i == 1 || i == toRow || j == 1 || j == toCol) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
 public class HollowRectangle {
-    
+    public static void main(String[] args) {
+        Code c = new Code();
+        c.hollowRectangle(10,5);
+        
+    }
 }
